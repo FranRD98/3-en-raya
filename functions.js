a0dfc1a6eb9771fcc64f7ddd048389ac7b2184d5
Fran <franrd98@outlook.com>
2024-10-28 23:32:02 +0100

    Drag & Drop funcional, a falta de bloquear enviar fichas al contrario y añadir altura fija del contenedor para que no baile

--- functions.js
+++ functions.js
@@ -1,23 +1,39 @@
+// 1. Obtenemos todas las fichas (tanto jugadores como del tablero)
 const fichas = document.querySelectorAll('.ficha');
 
-console.log(fichas);
-
+// 2. A cada ficha le asignamos el atributo 'draggable', el evento 'DragStart', 'dragover' y el evento 'drop'
 fichas.forEach(ficha => {
-    ficha.setAttribute('draggable', true);
-    ficha.addEventListener('dragstart', dragstart);
-    ficha.addEventListener('drop', drop);
+    ficha.setAttribute('draggable', true); // Permitirá hacer drag
+    ficha.addEventListener('dragstart', dragStart); // Se iniciará al iniciar el drag
+    ficha.addEventListener('dragover', dragOver); // Se activa al ponerse encima de la zona draggeable
+    ficha.addEventListener('dragleave', dragLeave); // De activa al salir de la zona draggeable
+    ficha.addEventListener('drop', drop); // Se activa al soltar el drag
 });
 
-function dragstart(e){
+// Obtenemos la imagen mediante el ID (fichaA1.. fichaB2...)
+function dragStart(e){
     e.dataTransfer.setData('text/plain', e.target.id);
-    console.log(e.target.id);
 }
 
+// Evitamos que el navegador interfiera y al ponerlos encima habilitamos una clase CSS
+function dragOver(e){
+    e.preventDefault();
+    e.target.classList.add('drag-over');
+}
+
+// Evitamos que el navegador interfiera y al salir de la zona draggeable quitamos la clase CSS
+function dragLeave(e){
+    e.target.classList.remove('drag-over');
+}
+
+// Insertamos la imagen en la ficha draggeable (tablero)
 function drop(e){
     e.preventDefault();
+    console.log(e.target);
     
     const id = e.dataTransfer.getData('text/plain'); // Recuperamos el id del elemento
     const draggable = document.getElementById(id);
+    e.target.classList.remove('drag-over');
 
     e.target.appendChild(draggable);
 }

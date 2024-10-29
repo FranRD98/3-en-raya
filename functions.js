a00518b282cd9059da566c0fef9e17797e2ecc26
MiguelAngel <miguelgiraldo1116@gmail.com>
2024-10-29 12:11:23 -0400

    Asignacion turno

--- functions.js
+++ functions.js
@@ -1,6 +1,8 @@
 // 1. Obtenemos todas las fichas (tanto jugadores como del tablero)
 const fichas = document.querySelectorAll('.ficha');
 
+let ultimaFicha = null;
+
 // 2. A cada ficha le asignamos el atributo 'draggable', el evento 'DragStart', 'dragover' y el evento 'drop'
 fichas.forEach(ficha => {
     ficha.setAttribute('draggable', true); // Permitirá hacer drag
@@ -36,4 +38,24 @@ function drop(e){
     e.target.classList.remove('drag-over');
 
     e.target.appendChild(draggable);
+
+    if(id.startsWith('fichaA')){
+        ultimaFicha = 'A';
+    } else if(id.startsWith('fichaB')){
+        ultimaFicha = 'B';    
+    }
+   
+    // Actualizamos el turno en el título del HTML con una imagen
+    const turnoElement = document.getElementById("turno");
+    const imagenTurno = document.createElement("img");
+
+    // Cambia la fuente de la imagen dependiendo del turno
+    imagenTurno.src = (ultimaFicha === 'A') ? 'imagenes/o.jpg' : 'imagenes/x.jpg';
+    imagenTurno.alt = `Turno del jugador ${(ultimaFicha === 'A') ? 'B' : 'A'}`;
+    imagenTurno.style.width = '50px'; 
+
+    // Limpiamos el contenido anterior y añadimos la nueva imagen
+    turnoElement.innerHTML = ""; // limpia el contenido anterior
+    turnoElement.appendChild(imagenTurno);
 }
+
